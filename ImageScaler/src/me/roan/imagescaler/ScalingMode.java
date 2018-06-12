ad61521ed3534a09bb89fd43d1ce71ee30ad81d3
Roan <roan.hofland@hotmail.com>
2018-06-12 20:47:33 +0200

    Add a javadoc to the ScalingMode class

--- ImageScaler/src/me/roan/imagescaler/ScalingMode.java
+++ ImageScaler/src/me/roan/imagescaler/ScalingMode.java
@@ -2,14 +2,35 @@ package me.roan.imagescaler;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Enum listing the available image scaling modes
+ * @author Roan
+ */
 public enum ScalingMode {
-
+	/**
+	 * Indicates the quality over speed algorithm
+	 */
 	QUALITY("Quality", BufferedImage.SCALE_SMOOTH),
+	/**
+	 * Indicates the speed over quality algorithm
+	 */
 	FAST("Speed", BufferedImage.SCALE_FAST);
 	
+	/**
+	 * Display name of the algorithm
+	 */
 	private final String name;
+	/**
+	 * Identifier of the algorithm
+	 */
 	protected final int mode;
 	
+	/**
+	 * Constructs a new ScalingMode
+	 * with the given name and mode identifier
+	 * @param name The display name of the algorithm
+	 * @param mode The mode identifier of the algorithm
+	 */
 	private ScalingMode(String name, int mode){
 		this.name = name;
 		this.mode = mode;
