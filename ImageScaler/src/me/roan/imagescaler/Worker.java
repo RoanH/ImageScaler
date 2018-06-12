b7d1bc847880c51309bd7a0f8f635c05bdcdf8d5
Roan <roan.hofland@hotmail.com>
2018-06-13 00:28:55 +0200

    Add a javadoc to the Worker class

--- ImageScaler/src/me/roan/imagescaler/Worker.java
+++ ImageScaler/src/me/roan/imagescaler/Worker.java
@@ -10,22 +10,47 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.PatternSyntaxException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
 
+/**
+ * Class responsible for actually scaling all
+ * the images using the indicated number of threads
+ * @author Roan
+ */
 public class Worker {
-	
+	/**
+	 * Number of images that have been scaled so far
+	 */
 	protected static final AtomicInteger completed = new AtomicInteger(0);
+	/**
+	 * Whether or not we are currently running or paused
+	 */
 	protected static volatile boolean running = false;
+	/**
+	 * List of files to scale
+	 */
 	private static List<File> files;
 	
-	public static final int prepare() throws PatternSyntaxException{
+	/**
+	 * Reads the list of images to rescale
+	 * from the selected input directory
+	 * and returns the number of images
+	 * that are going to be rescaled
+	 * @return The number of images that
+	 *         are going to be rescaled
+	 */
+	public static final int prepare(){
 		files = getImages(Main.inputDir);
 		return files.size();
 	}
 
+	/**
+	 * Starts the threads and rescales all the images
+	 * @param listener The ProgressListener to notify of
+	 *        any progress that's made
+	 */
 	public static final void start(ProgressListener listener){
 		ExecutorService executor = Executors.newFixedThreadPool(Main.threads);
 		completed.set(0);
@@ -54,12 +79,24 @@ public class Worker {
 		}
 	}
 	
+	/**
+	 * Simply interface that gets called when progress was made
+	 * @author Roan
+	 */
 	@FunctionalInterface
 	protected static abstract interface ProgressListener{
-		
+		/**
+		 * Called when progress was made
+		 */
 		public abstract void progress();
 	}
 	
+	/**
+	 * Rescales the given image in accordance with the
+	 * selected options.
+	 * @param file The image to rescale
+	 * @throws IOException When an IOException occurs
+	 */
 	private static final void scale(File file) throws IOException{
 		BufferedImage img = ImageIO.read(file);
 		Image scaled = img.getScaledInstance((int)Math.round((double)img.getWidth() * Main.scale), (int)Math.round((double)img.getHeight() * Main.scale), Main.mode.mode);
@@ -77,6 +114,11 @@ public class Worker {
 		data.flush();
 	}
 	
+	/**
+	 * Converts the given image to a BufferedImage
+	 * @param img The image to convert
+	 * @return The converted image
+	 */
 	private static final BufferedImage toBufferedImage(Image img){
 		BufferedImage buffer = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
 		Graphics g = buffer.createGraphics();
@@ -85,7 +127,13 @@ public class Worker {
 		return buffer;
 	}
 	
-	private static final List<File> getImages(File dir) throws PatternSyntaxException{
+	/**
+	 * Gets a list of all the files in the
+	 * input folder that math the filter regex
+	 * @param dir The directory to search
+	 * @return The list of matching image files
+	 */
+	private static final List<File> getImages(File dir){
 		List<File> list = new ArrayList<File>();
 		for(File file : dir.listFiles()){
 			if(file.isDirectory()){
