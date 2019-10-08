79373daac710aec10aa14f698f47000512c0c008
Roan <roan.hofland@hotmail.com>
2019-10-09 00:23:12 +0200

    Switch to Util version checking

--- ImageScaler/src/me/roan/imagescaler/Main.java
+++ ImageScaler/src/me/roan/imagescaler/Main.java
@@ -3,12 +3,8 @@ package me.roan.imagescaler;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -32,6 +28,7 @@ import javax.swing.UIManager;
 
 import me.roan.util.ClickableLink;
 import me.roan.util.Dialog;
+import me.roan.util.Util;
 
 /**
  * Relatively simple program that rescales images
@@ -311,19 +308,13 @@ public class Main{
 
 		JPanel version = new JPanel(new GridLayout(2, 1, 0, 2));
 		version.setBorder(BorderFactory.createTitledBorder("Information"));
-		JLabel ver = new JLabel("<html><center><i>Version: v2.2, latest version: <font color=gray>loading</font></i></center></html>", SwingConstants.CENTER);
-		version.add(ver);
-		new Thread(()->{
-			String v = checkVersion();//XXX the version number - don't forget build.gradle
-			ver.setText("<html><center><i>Version: v2.2, latest version: " + (v == null ? "unknown :(" : v) + "</i></center></html>");
-		}, "Version Checker").start();
 		JPanel links = new JPanel(new GridLayout(1, 2, -2, 0));
 		JLabel forum = new JLabel("<html><font color=blue><u>Forums</u></font> -</html>", SwingConstants.RIGHT);
 		JLabel git = new JLabel("<html>- <font color=blue><u>GitHub</u></font></html>", SwingConstants.LEFT);
 		links.add(forum);
 		links.add(git);
 		version.add(links);
-		version.add(ver);
+		version.add(Util.getVersionLabel("ImageScaler", "v2.2"));//XXX the version number - don't forget build.gradle
 		forum.addMouseListener(new ClickableLink("https://osu.ppy.sh/community/forums/topics/762684"));
 		git.addMouseListener(new ClickableLink("https://github.com/RoanH/ImageScaler"));
 
@@ -341,44 +332,5 @@ public class Main{
 		frame.setSize(400, panel.getPreferredSize().height + frame.getInsets().top + frame.getInsets().bottom);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-	}
-
-	/**
-	 * Checks the Image Scaler version to see
-	 * if we are running the latest version
-	 * @return The latest version
-	 */
-	private static final String checkVersion(){
-		try{
-			HttpURLConnection con = (HttpURLConnection)new URL("https://api.github.com/repos/RoanH/ImageScaler/tags").openConnection();
-			con.setRequestMethod("GET");
-			con.addRequestProperty("Accept", "application/vnd.github.v3+json");
-			con.setConnectTimeout(10000);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			String line = reader.readLine();
-			reader.close();
-			String[] versions = line.split("\"name\":\"v");
-			int max_main = 1;
-			int max_sub = 0;
-			String[] tmp;
-			for(int i = 1; i < versions.length; i++){
-				tmp = versions[i].split("\",\"")[0].split("\\.");
-				if(Integer.parseInt(tmp[0]) > max_main){
-					max_main = Integer.parseInt(tmp[0]);
-					max_sub = Integer.parseInt(tmp[1]);
-				}else if(Integer.parseInt(tmp[0]) < max_main){
-					continue;
-				}else{
-					if(Integer.parseInt(tmp[1]) > max_sub){
-						max_sub = Integer.parseInt(tmp[1]);
-					}
-				}
-			}
-			return "v" + max_main + "." + max_sub;
-		}catch(Exception e){
-			return null;
-			//No Internet access or something else is wrong,
-			//No problem though since this isn't a critical function
-		}
 	}
 }
