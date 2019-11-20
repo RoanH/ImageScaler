00bf26a8b5b17d901405fc278fa94bb56f1df4c4
Roan Hofland <roan.hofland@hotmail.com>
2019-11-20 18:18:37 +0100

    Add a javadoc and clean up the FolderSelector class

--- ImageScaler/src/me/roan/imagescaler/FolderSelector.java
+++ ImageScaler/src/me/roan/imagescaler/FolderSelector.java
@@ -24,23 +24,47 @@ import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+/**
+ * Represents a small component used to select a folder
+ * or file. Folders or files can be select with this
+ * component either by trying, dropping or pasting.
+ * @author Roan
+ */
 public class FolderSelector extends JPanel implements DropTargetListener, DocumentListener, ActionListener{
 	/**
 	 * Serial ID.
 	 */
 	private static final long serialVersionUID = 8349454444582863534L;
 	/**
-	 * The file chooser that is used
+	 * The file chooser that is used.
 	 */
 	protected static JFileChooser chooser;
+	/**
+	 * Consumer that gets notified when the select folder or file changes.
+	 */
 	private Consumer<String> listener;
+	/**
+	 * Text field to display and directly modify the selected element.
+	 */
 	private JTextField field = new JTextField("");
+	/**
+	 * Button to open the file chooser.
+	 */
 	private JButton select = new JButton("Select");
 	
+	/**
+	 * Constructs a new FolderSelector.
+	 */
 	public FolderSelector(){
 		this(null);
 	}
 	
+	/**
+	 * Constructs a new FolderSelector with the
+	 * given update listener.
+	 * @param listener The listener to send selection
+	 *        changes to (can be <code>null</code>).
+	 */
 	public FolderSelector(Consumer<String> listener){
 		super(new BorderLayout());
 		
@@ -55,16 +79,50 @@ public class FolderSelector extends JPanel implements DropTargetListener, Docume
 		field.getDocument().addDocumentListener(this);
 	}
 	
+	/**
+	 * Sets the listener to send selection updates to.
+	 * @param listener The listener to send selection
+	 *        changes to (can be <code>null</code>).
+	 */
 	public void setListener(Consumer<String> listener){
 		this.listener = listener;
 	}
 	
+	/**
+	 * Gets the selected element as a string.
+	 * @return The select target element
+	 *         (file or folder).
+	 */
 	public String getText(){
 		return field.getText();
 	}
 	
+	/**
+	 * Gets the selected target as a file.
+	 * @return The selected target.
+	 */
+	public File getFile(){
+		return new File(getText());
+	}
+	
+	/**
+	 * Sets the select element to the
+	 * given string.
+	 * @param text The new selected element.
+	 */
 	public void setText(String text){
 		field.setText(text);
+	}
+	
+	/**
+	 * Sends a content update to the
+	 * listener if one is set.
+	 * @see #setListener(Consumer)
+	 */
+	private void update(){
+		if(listener != null){
+			listener.accept(field.getText());
+		}
 	}
 	
 	@Override
@@ -83,23 +141,17 @@ public class FolderSelector extends JPanel implements DropTargetListener, Docume
 	
 	@Override
 	public void insertUpdate(DocumentEvent e){
-		if(listener != null){
-			listener.accept(field.getText());
-		}	
+		update();
 	}
 
 	@Override
 	public void removeUpdate(DocumentEvent e){
-		if(listener != null){
-			listener.accept(field.getText());
-		}
+		update();
 	}
 
 	@Override
 	public void changedUpdate(DocumentEvent e){
-		if(listener != null){
-			listener.accept(field.getText());
-		}
+		update();
 	}
 
 	@Override
@@ -125,7 +177,6 @@ public class FolderSelector extends JPanel implements DropTargetListener, Docume
 				dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
 				@SuppressWarnings("unchecked")
 				List<File> files = (List<File>)dtde.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
-				System.out.println("DND: " + files);
 				if(files.size() > 0){
 					field.setText(files.get(0).getAbsolutePath());
 				}
